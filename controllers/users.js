a97bb88e4de2001c1e5e60864bc34c124fab38b7
Enrico <enricogarozzo@hotmail.it>
2026-02-20 20:39:04 +0100

    redirect signup and login session

--- controllers/users.js
+++ controllers/users.js
@@ -1,18 +1,27 @@
 const User = require('../models/user');
 
+//render registration form
+
 module.exports.renderRegister = (req,res)=>{
     res.render('users/register');
 }
 
+//handle registration
 module.exports.register = async (req,res)=>{
     try{
         const {email, username, password } = req.body;
         const user = new User({email, username});
         const registeredUser = await User.register(user,password);
+
+        //log the user in immediately
         req.login(registeredUser, err => {
             if (err) return next(err);
             req.flash('success','Welcome to Yelpcamp');
-            res.redirect('/campgrounds'); 
+
+            //redirect to originally requested page or campgrounds
+            const redirectUrl = req.session.returnTo || '/campgrounds';
+            delete req.session.returnTo;
+            res.redirect(redirectUrl);
         })
        
     }catch(e){
@@ -27,9 +36,16 @@ module.exports.renderLogin = (req,res)=>{
 
 module.exports.login =  (req, res) => {
     req.flash('success', 'Welcome back!');
-    const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
+
+    //redirect to originally requested page or campgrounds
+    //ignore the commented out line
+    //const redirectUrl = res.locals.returnTo || '/campgrounds'; // update this line to use res.locals.returnTo now
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
     res.redirect(redirectUrl);
 }
+
+//handle logout
 
 module.exports.logout = (req,res, next)=>{
     req.logout(function (err){
